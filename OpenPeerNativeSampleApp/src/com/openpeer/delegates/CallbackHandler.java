1fb10c4bc3f04eea3ded73cf9631e4253a0bcce3
Bojan Lazic <bojan@hookflash.com>
2014-01-20 17:58:25 +0100

    Java Callback Handler added. This is a class that is a glue layer between JNI C++ callbacks, and Java events. Account, Stack and Identity glue is implemented.

--- OpenPeerNativeSampleApp/src/com/openpeer/delegates/CallbackHandler.java
+++ OpenPeerNativeSampleApp/src/com/openpeer/delegates/CallbackHandler.java
@@ -1,43 +1,60 @@
 package com.openpeer.delegates;
 
+import java.util.ArrayList;
+
 import com.openpeer.javaapi.AccountStates;
 import com.openpeer.javaapi.IdentityStates;
 import com.openpeer.javaapi.OPAccount;
 import com.openpeer.javaapi.OPAccountDelegate;
 import com.openpeer.javaapi.OPIdentity;
 import com.openpeer.javaapi.OPIdentityDelegate;
+import com.openpeer.javaapi.OPStack;
 import com.openpeer.javaapi.OPStackDelegate;
 
 public class CallbackHandler{
+	
+	//OPAccountDelegate support
+	private OPAccount mAccount;
+	ArrayList<OPAccountDelegate> accountDelegates = new ArrayList<OPAccountDelegate> ();
 
-	public static void onJniCallback()
-	{
-		int i = 0;
-		i++;
-	}
-	//Account Delegate static methods
-	public static void onAccountStateChanged(int state) {
-		// TODO Auto-generated method stub
-		if (mAccount != null && mAccountDelegate != null)
+	//Account Delegate glue methods
+	public void onAccountStateChanged(int state) {
+		for (OPAccountDelegate delegate : accountDelegates)
 		{
-			mAccountDelegate.onAccountStateChanged(mAccount, AccountStates.values()[state] );
+			if (mAccount != null && delegate != null)
+			{
+				delegate.onAccountStateChanged(mAccount, AccountStates.values()[state] );
+			}
+		}
+	}
+
+	public void onAccountAssociatedIdentitiesChanged(OPAccount account) {
+
+		for (OPAccountDelegate delegate : accountDelegates)
+		{
+			if (mAccount != null && delegate != null)
+			{
+				delegate.onAccountAssociatedIdentitiesChanged(mAccount);
+			}
 		}
 		
 	}
 
-	public static void onAccountAssociatedIdentitiesChanged(OPAccount account) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static void onAccountPendingMessageForInnerBrowserWindowFrame(
+	public void onAccountPendingMessageForInnerBrowserWindowFrame(
 			OPAccount account) {
-		// TODO Auto-generated method stub
+
+		for (OPAccountDelegate delegate : accountDelegates)
+		{
+			if (mAccount != null && delegate != null)
+			{
+				delegate.onAccountPendingMessageForInnerBrowserWindowFrame(mAccount);
+			}
+		}
 		
 	}
 	
 	//Account delegate register/unregister methods
-	public static boolean registerAccountDelegate(OPAccount account, OPAccountDelegate delegate)
+	public boolean registerAccountDelegate(OPAccount account, OPAccountDelegate delegate)
 	{
 		if (account == null || delegate == null)
 		{
@@ -49,64 +66,89 @@ public class CallbackHandler{
 			mAccount = account;
 		}
 		
-		if (mAccountDelegate == null)
-		{
-			mAccountDelegate = delegate;
-		}
+		// Store the delegate object
+        this.accountDelegates.add(delegate);
 		
 		return true;
 	}
 	
-	public static void unregisterAccountDelegate()
+	public void unregisterAccountDelegate(OPAccountDelegate delegate)
 	{
 		mAccount = null;
-		mAccountDelegate = null;
+		this.accountDelegates.remove(delegate);
 		
 	}
 
-	public static void onStackShutdown() {
-		// TODO Auto-generated method stub
+	/////////////////////////////////////////////////////////////////////
+	// STACK DELEGATE GLUE
+	/////////////////////////////////////////////////////////////////////
+	//OPAccountDelegate support
+	//private OPStack mStack;
+	ArrayList<OPStackDelegate> stackDelegates = new ArrayList<OPStackDelegate> ();
+	
+	public void onStackShutdown() {
+		for (OPStackDelegate delegate : stackDelegates)
+		{
+			if (delegate != null)
+			{
+				delegate.onStackShutdown();
+			}
+		}
 		
 	}
 	
 	//Stack delegate register/unregister methods
-	public static boolean registerStackDelegate(OPStackDelegate delegate)
+	public boolean registerStackDelegate(OPStackDelegate delegate)
 	{
 		if (delegate == null)
 		{
 			return false;
 		}
 		
-		if (mStackDelegate == null)
-		{
-			mStackDelegate = delegate;
-		}
+		// Store the delegate object
+        this.stackDelegates.add(delegate);
 		
 		return true;
 	}
 	
-	public static void unregisterStackDelegate()
+	public void unregisterStackDelegate(OPStackDelegate delegate)
 	{
-		mStackDelegate = null;
+		this.stackDelegates.remove(delegate);
 		
 	}
 
-	public static void onIdentityStateChanged(int state) {
-		// TODO Auto-generated method stub
-		if (mIdentity != null && mIdentityDelegate != null)
+	
+	/////////////////////////////////////////////////////////////////////
+	// IDENTITY DELEGATE GLUE
+	/////////////////////////////////////////////////////////////////////
+	//OPAccountDelegate support
+	private OPIdentity mIdentity;
+	ArrayList<OPIdentityDelegate> identityDelegates = new ArrayList<OPIdentityDelegate> ();
+	
+	public void onIdentityStateChanged(int state) {
+		
+		for (OPIdentityDelegate delegate : identityDelegates)
 		{
-			mIdentityDelegate.onIdentityStateChanged(mIdentity, IdentityStates.values()[state] );
+			if (mIdentity != null && delegate != null)
+			{
+				delegate.onIdentityStateChanged(mIdentity, IdentityStates.values()[state] );
+			}
 		}
-		
 	}
 
-	public static void onIdentityPendingMessageForInnerBrowserWindowFrame() {
-		// TODO Auto-generated method stub
+	public void onIdentityPendingMessageForInnerBrowserWindowFrame() {
+		for (OPIdentityDelegate delegate : identityDelegates)
+		{
+			if (mIdentity != null && delegate != null)
+			{
+				delegate.onIdentityPendingMessageForInnerBrowserWindowFrame(mIdentity);
+			}
+		}
 		
 	}
 	
 	//Identity delegate register/unregister methods
-	public static boolean registerIdentityDelegate(OPIdentity identity, OPIdentityDelegate delegate)
+	public boolean registerIdentityDelegate(OPIdentity identity, OPIdentityDelegate delegate)
 	{
 		if (identity == null || delegate == null)
 		{
@@ -118,26 +160,23 @@ public class CallbackHandler{
 			mIdentity = identity;
 		}
 		
-		if (mIdentityDelegate == null)
-		{
-			mIdentityDelegate = delegate;
-		}
+		// Store the delegate object
+        this.identityDelegates.add(delegate);
 		
 		return true;
 	}
 	
-	public static void unregisterIdentityDelegate()
+	public void unregisterIdentityDelegate(OPIdentityDelegate delegate)
 	{
 		mIdentity = null;
-		mIdentityDelegate = null;
+		this.identityDelegates.remove(delegate);
 		
 	}
 	
-	private static OPAccountDelegate mAccountDelegate;
-	private static OPAccount mAccount;
-	
-	private static OPStackDelegate mStackDelegate;
-	
-	private static OPIdentityDelegate mIdentityDelegate;
-	private static OPIdentity mIdentity;
+	public static void onJniCallback()
+	{
+		int i = 0;
+		i++;
+	}
+
 }
