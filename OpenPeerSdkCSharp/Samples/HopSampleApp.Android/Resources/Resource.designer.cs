15b22be5788e3d7fbd069933feaaf76b4fa51fa2
Robin Raymond <robin@hookflash.com>
2014-02-23 12:48:27 -0500

    - tweaked layout of contact avatar, made default avatar a generic person https://github.com/openpeer/opandroid/issues/88
    - removed dead / bogus files

--- OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Resources/Resource.designer.cs
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Resources/Resource.designer.cs
@@ -222,43 +222,46 @@ namespace HopSampleApp
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int chat = 2130837504;
+			public const int avatar = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int chat_alt = 2130837505;
+			public const int chat = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int chat_bubble_left = 2130837506;
+			public const int chat_alt = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int chat_bubble_left_source = 2130837507;
+			public const int chat_bubble_left = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int chat_bubble_right = 2130837508;
+			public const int chat_bubble_left_source = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int chat_bubble_right_source = 2130837509;
+			public const int chat_bubble_right = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int Icon = 2130837510;
+			public const int chat_bubble_right_source = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int progress_medium_holo = 2130837511;
+			public const int Icon = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int ptrsharp_serious_business_gradient = 2130837512;
+			public const int progress_medium_holo = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int serious_business_arrow = 2130837513;
+			public const int ptrsharp_serious_business_gradient = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int serious_business_arrow2 = 2130837514;
+			public const int serious_business_arrow = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int spinner_20_inner_holo = 2130837515;
+			public const int serious_business_arrow2 = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int spinner_20_outer_holo = 2130837516;
+			public const int spinner_20_inner_holo = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int spinner_20_outer_holo = 2130837517;
 			
 			static Drawable()
 			{
