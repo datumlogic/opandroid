271bdcfd9f603fc4f2e2b66db3d1699e6a37f074
Petar Srdanovic <petar.srdanovic88@gmail.com>
2014-03-25 13:00:49 +0100

    layout for incoming voice call

---
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/PopupCallActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HopSampleApp
+{
+	[Activity (Theme = "@style/Theme.Splash",MainLauncher = false,NoHistory = true,Icon="@drawable/op")]			
+	public class PopupCallActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			SetContentView (Resource.Layout.PopupCall);
+		}
+	}
+}
+
