697946dec1c3220be9445d9d45186868c129c650
Petar Srdanovic <petar.srdanovic88@gmail.com>
2014-03-26 12:51:21 +0100

    all staff for video call.events etc..

--- OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/PoupVideoCallActivity.cs
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/PoupVideoCallActivity.cs
@@ -8,10 +8,19 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Net;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+using System.Drawing;
+using System.IO;
+using Java.Net;
+using System.Threading.Tasks;
+using Android.Net;
+
 
 namespace HopSampleApp
 {
-	[Activity (Label = "PoupVideoCallActivity")]			
+	[Activity (Theme = "@style/Theme.Splash",MainLauncher = false,NoHistory = true,Icon="@drawable/op")]			
 	public class PoupVideoCallActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -19,6 +28,47 @@ namespace HopSampleApp
 			base.OnCreate (bundle);
 
 			// Create your application here
+			SetContentView (Resource.Layout.PopupCall);
+
+			var imageBitmap = GetImageBitmapFromUrl("http://xamarin.com/resources/design/home/devices.png");//Download avatar image
+
+			ImageView avatar = (ImageView)FindViewById(Resource.Id.ImageVideoCallAvatar);//Avatar image for user
+			avatar.SetImageBitmap(imageBitmap);//Set avatar
+
+			TextView UserIdTop = (TextView)FindViewById (Resource.Id.VideoCallerIDTop);//User name id located at the top
+			TextView UserIdCenter = (TextView)FindViewById (Resource.Id.CallerVideoIDCenter);//User name id located at the bottom
+			TextView CallInfo = (TextView)FindViewById (Resource.Id.videocallInfo);//Call info for incoming and outgoing calls
+
+			CallInfo.Text = "Incoming video call from";//Set call info for incoming and outgoing calls
+			UserIdTop.Text = "Petar Srdanovic";//Set user id top
+			UserIdCenter.Text = "Petar Srdanovic";//Set user id bottom
+
+			Button AnswerButton = (Button)FindViewById (Resource.Id.btnAnswerVideoCall);
+			AnswerButton.Click += delegate {
+				Console.WriteLine("Answer button");
+				//Logic for Answer to call
+			};
+
+			Button DeclineButton = (Button)FindViewById (Resource.Id.btnDeclineVideoCall);
+			DeclineButton.Click += delegate {
+
+				Console.WriteLine("Decline button");
+			};
+		}
+		private Bitmap GetImageBitmapFromUrl(string url)
+		{
+			Bitmap imageBitmap = null;
+
+			using (var webClient = new WebClient())
+			{
+				var imageBytes = webClient.DownloadData(url);
+				if (imageBytes != null && imageBytes.Length > 0)
+				{
+					imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+				}
+			}
+
+			return imageBitmap;
 		}
 	}
 }
