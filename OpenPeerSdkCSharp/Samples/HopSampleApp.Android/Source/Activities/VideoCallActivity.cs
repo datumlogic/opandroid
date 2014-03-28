ddb175f7a743a8a2bdd2060917eda6f2e95aede6
Petar Srdanovic <petar.srdanovic88@gmail.com>
2014-03-28 13:26:09 +0100

    implemented events

--- OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/VideoCallActivity.cs
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/VideoCallActivity.cs
@@ -11,14 +11,40 @@ using Android.Widget;
 
 namespace HopSampleApp
 {
-	[Activity (Label = "VideoCallActivity")]			
+
+	[Activity (Theme = "@style/Theme.Splash",MainLauncher = false,NoHistory = true,Icon="@drawable/op",ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]			
 	public class VideoCallActivity : Activity
 	{
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			// Create your application here
+			SetContentView (Resource.Layout.VideoCall);
+
+			LinearLayout localViewLayout = FindViewById<LinearLayout> (Resource.Id.myLocalViewLinearLayout);//Local cam view
+			LinearLayout remoteViewLayout = FindViewById<LinearLayout> (Resource.Id.myRemoteViewLinearLayout);//Remote cam view
+
+			ImageButton SwichCam = FindViewById<ImageButton> (Resource.Id.ButtonSwitchCam);//SwichCam button
+			ImageButton MuteMic = FindViewById<ImageButton> (Resource.Id.ButtonMuteMic);//Mute mic button
+			ImageButton StartChat = FindViewById<ImageButton> (Resource.Id.ButtonStartChat);//StartChat button
+
+			//switching from the front to the back camera
+			SwichCam.Click += delegate {
+				Console.WriteLine("Swich camera");
+			};
+			//Mute mic
+			MuteMic.Click += delegate {
+
+				Console.WriteLine("Mute mic");
+			};
+			//Start chat with current user
+			StartChat.Click += delegate {
+
+				Console.WriteLine("Start Chat with user");
+			};
+
 		}
 	}
 }
