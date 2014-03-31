edea030853a27ebb59f4b7539a07df2ace91fc50
Petar Srdanovic <petar.srdanovic88@gmail.com>
2014-03-31 11:31:04 +0200

    ending calls activity

--- OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/EndCallActivity.cs
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Activities/EndCallActivity.cs
@@ -11,7 +11,7 @@ using Android.Widget;
 
 namespace HopSampleApp
 {
-	[Activity (Label = "EndCallActivity")]			
+	[Activity (Theme = "@style/Theme.Splash",MainLauncher = false,NoHistory = true,Icon="@drawable/op",ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]		
 	public class EndCallActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -19,6 +19,32 @@ namespace HopSampleApp
 			base.OnCreate (bundle);
 
 			// Create your application here
+			SetContentView (Resource.Layout.EndCall);
+			TextView DurationCall = FindViewById<TextView> (Resource.Id.DurationText);
+			ImageButton MicButton = FindViewById<ImageButton> (Resource.Id.MicButton);
+			ImageButton SpikerButton = FindViewById<ImageButton> (Resource.Id.SpikerButton);
+			ImageButton AddButton = FindViewById<ImageButton> (Resource.Id.AddButton);
+			ImageButton RecordingButton = FindViewById<ImageButton> (Resource.Id.RecordButton);
+
+			string time="00:00:05";
+			DurationCall.Text = String.Format ("Duracion:{0}",time);
+
+			MicButton.Click += delegate {
+			
+				Console.WriteLine("Mic button");
+			};
+			SpikerButton.Click += delegate {
+			
+				Console.WriteLine("Spiker button");
+			};
+			AddButton.Click += delegate {
+			
+				Console.WriteLine("Add button");
+			};
+			RecordingButton.Click += delegate {
+			
+				Console.WriteLine("Recording button");
+			};
 		}
 	}
 }
