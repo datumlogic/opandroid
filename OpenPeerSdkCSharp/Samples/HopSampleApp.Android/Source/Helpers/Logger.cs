fb740e75a199dc992e645b6e094a474b57760584
Petar Srdanovic <petar.srdanovic88@gmail.com>
2014-04-02 11:21:15 +0200

    fix

---
+++ OpenPeerSdkCSharp/Samples/HopSampleApp.Android/Source/Helpers/Logger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace OpenPeerSdk
+{
+	namespace Helpers
+	{
+		public class Logger
+		{
+			public enum Level
+			{
+				None,
+				Basic,
+				Detail,
+				Debug,
+				Trace,
+				Insane
+			}
+
+			public enum Severity
+			{
+				Informational,
+				Warning,
+				Error,
+				Fatal
+			}
+
+			private static Internal.Logger singleton;
+
+			static Logger ()
+			{
+				singleton = new Internal.Logger ();
+			}
+
+			public static void Basic (string message, params object[] list)
+			{
+				singleton.Basic (message, list);
+			}
+
+			public static void Detail (string message, params object[] list)
+			{
+				singleton.Detail (message, list);
+			}
+
+			public static void Debug (string message, params object[] list)
+			{
+				singleton.Debug (message, list);
+			}
+
+			public static void Trace (string message, params object[] list)
+			{
+				singleton.Trace (message, list);
+			}
+
+			public static void Insane (string message, params object[] list)
+			{
+				singleton.Insane (message, list);
+			}
+
+			public static void Warning (Level level, string message, params object[] list)
+			{
+				singleton.Warning (level, message, list);
+			}
+
+			public static void Error (Level level, string message, params object[] list)
+			{
+				singleton.Error (level, message, list);
+			}
+
+			public static void Fatal (Level level, string message, params object[] list)
+			{
+				singleton.Fatal (level, message, list);
+			}
+
+			public static void RegisterCurrentNamespace()
+			{
+				singleton.RegisterCurrentNamespace ();
+			}
+
+			public static void RegisterCurrentNamespace(string subsystemName)
+			{
+				singleton.RegisterCurrentNamespace (subsystemName);
+			}
+
+			public static void RegisterNamespace(string @namespace, string subsystemName)
+			{
+				singleton.RegisterNamespace (@namespace, subsystemName);
+			}
+		}
+
+	}
+}
