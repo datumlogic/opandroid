255564ccce6b7be38efc307755c459f01070ea28
Bojan Lazic <lazic_bo@yahoo.com>
2014-04-03 11:42:17 +0200

    License text added to all openpeer files.

--- openpeer-android-sdk/jni/com_openpeer_javaapi_OPMediaEngine.cpp
+++ openpeer-android-sdk/jni/com_openpeer_javaapi_OPMediaEngine.cpp
@@ -1,3 +1,34 @@
+/*
+
+ Copyright (c) 2014, SMB Phone Inc.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies,
+ either expressed or implied, of the FreeBSD Project.
+
+ */
+
 #include "com_openpeer_javaapi_OPMediaEngine.h"
 #include "com_openpeer_javaapi_OPStackMessageQueue.h"
 #include "openpeer/core/IStack.h"
@@ -9,6 +40,8 @@
 #include <vie_base.h>
 
 #include "globals.h"
+
+//#define TEST_MEDIA_ENGINE
 
 using namespace openpeer::core;
 
@@ -52,14 +85,18 @@ JNIEXPORT jobject JNICALL Java_com_openpeer_javaapi_OPMediaEngine_singleton
 	jni_env = getEnv();
 	if(jni_env)
 	{
+#ifdef TEST_MEDIA_ENGINE
 		cls = findClass("com/openpeer/javaapi/test/OPTestMediaEngine");
 		method = jni_env->GetMethodID(cls, "<init>", "()V");
 		object = jni_env->NewObject(cls, method);
 
 	    openpeer::core::test::TestMediaEngineFactoryPtr overrideFactory(new openpeer::core::test::TestMediaEngineFactory);
-
 	    openpeer::core::internal::Factory::override(overrideFactory);
-
+#else
+		cls = findClass("com/openpeer/javaapi/OPMediaEngine");
+		method = jni_env->GetMethodID(cls, "<init>", "()V");
+		object = jni_env->NewObject(cls, method);
+#endif
 		mediaEnginePtr = IMediaEngine::singleton();
 
 	}
