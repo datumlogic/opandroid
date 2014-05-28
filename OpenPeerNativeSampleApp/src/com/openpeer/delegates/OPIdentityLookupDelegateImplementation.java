f16958f7b348601ba7ac3441e369443d392bbc34
Bojan Lazic <lazic_bo@yahoo.com>
2014-05-28 09:29:12 +0200

    Added lookup result fetch. https://github.com/openpeer/opandroid/issues/277

--- OpenPeerNativeSampleApp/src/com/openpeer/delegates/OPIdentityLookupDelegateImplementation.java
+++ OpenPeerNativeSampleApp/src/com/openpeer/delegates/OPIdentityLookupDelegateImplementation.java
@@ -1,9 +1,13 @@
 package com.openpeer.delegates;
 
+import java.util.List;
+
 import android.util.Log;
 
+import com.openpeer.javaapi.OPIdentityContact;
 import com.openpeer.javaapi.OPIdentityLookup;
 import com.openpeer.javaapi.OPIdentityLookupDelegate;
+import com.openpeer.javaapi.OPIdentityLookupInfo;
 
 public class OPIdentityLookupDelegateImplementation extends
 		OPIdentityLookupDelegate {
@@ -12,6 +16,9 @@ public class OPIdentityLookupDelegateImplementation extends
 	public void onIdentityLookupCompleted(OPIdentityLookup lookup) {
 		// TODO Auto-generated method stub
 		Log.d("output", "Identity lookup test PASSED");
+		List<OPIdentityContact> updated = lookup.getUpdatedIdentities();
+		List<OPIdentityLookupInfo> invalid = lookup.getInvalidIdentities();
+		List<OPIdentityLookupInfo> unchanged = lookup.getUnchangedIdentities();
 
 	}
 
