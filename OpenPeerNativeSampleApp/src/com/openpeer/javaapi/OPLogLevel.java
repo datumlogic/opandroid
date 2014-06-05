400fec4737c9436ddde656cca51c6f71fe0a67a8
Bojan Lazic <lazic_bo@yahoo.com>
2014-06-05 17:16:22 +0200

    Log level support added. Conversation thread fixes. Event manager fixes. https://github.com/openpeer/opandroid/issues/286 https://github.com/openpeer/opandroid/issues/297

--- OpenPeerNativeSampleApp/src/com/openpeer/javaapi/OPLogLevel.java
+++ OpenPeerNativeSampleApp/src/com/openpeer/javaapi/OPLogLevel.java
@@ -2,4 +2,10 @@ package com.openpeer.javaapi;
 
 public enum OPLogLevel {
 
+    LogLevel_None,     // no logging of any kind
+    LogLevel_Basic,    // most basic logging only
+    LogLevel_Detail,
+    LogLevel_Debug,
+    LogLevel_Trace,
+    LogLevel_Insane    // most detailed log level
 }
