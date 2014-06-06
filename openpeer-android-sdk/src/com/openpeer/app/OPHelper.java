6f5c3424b2f009a5e0e958b286f748ee7a135373
brucexia <bruce@hookflash.com>
2014-06-06 09:45:08 -0700

    #303 add app settings configuration file support. tested

--- openpeer-android-sdk/src/com/openpeer/app/OPHelper.java
+++ openpeer-android-sdk/src/com/openpeer/app/OPHelper.java
@@ -26,12 +26,20 @@ public class OPHelper {
 	private static final String TAG = OPHelper.class.getSimpleName();
 	private static OPHelper instance;
 	Context mContext;
+	static {
+		try {
+			System.loadLibrary("z_shared");
+			System.loadLibrary("openpeer");
 
+		} catch (UnsatisfiedLinkError use) {
+			use.printStackTrace();
+		}
+	}
 	private OPHelper(Context context) {
 		mContext = context;
 	}
 
-	public OPHelper getInstance(Context context) {
+	public static OPHelper getInstance(Context context) {
 		if (instance == null) {
 			instance = new OPHelper(context);
 		}
@@ -42,11 +50,10 @@ public class OPHelper {
 		OPLogger.installTelnetLogger(59999, 60, true);
 	}
 	public void init() {
-		OPSdkConfig.init(mContext);
 		//TODO: Add delegate when implement mechanism to post events to the android GUI thread
 		OPStackMessageQueue stackMessageQueue = OPStackMessageQueue.singleton(); 
-		stackMessageQueue = new OPStackMessageQueue();
-		stackMessageQueue.interceptProcessing(null);
+//		stackMessageQueue = new OPStackMessageQueue();
+//		stackMessageQueue.interceptProcessing(null);
 		OPStack stack = OPStack.singleton();
 		
 		//		
@@ -62,8 +69,7 @@ public class OPHelper {
 		String forceDashSettings = createForceDashSetting();
 		OPSettings.apply(forceDashSettings);
 		
-		String appSettings = createFakeApplicationSettings();
-		OPSettings.apply(appSettings);
+		OPSettings.apply(OPSdkConfig.getInstance(mContext).getAPPSettingsString());
 		
 		//TODO: After interception is done, we can call setup
 		
@@ -94,50 +100,6 @@ public class OPHelper {
             JSONObject jsonObject = new JSONObject();
 
             jsonObject.put("openpeer/core/authorized-application-id-split-char", "-");
-            parent.put("root", jsonObject);
-            Log.d("output", parent.toString(2));
-            return parent.toString(2);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return "";
-        }
-	}
-	
-	private String createFakeApplicationSettings()
-	{
-		//TODO: Read configuration from file
-	
-//		{
-//			"root": {
-//				"outerFrameURL": "http://jsouter-v1-beta-1-i.hcs.io/identity.html?view=choose&federated=false",
-//				"identityProviderDomain": "identity-v1-beta-1-i.hcs.io",
-//				"identityFederateBaseURI": "identity://identity-v1-beta-1-i.hcs.io/",
-//				"namespaceGrantServiceURL": "http://jsouter-v1-beta-1-i.hcs.io/grant.html",
-//				"lockBoxServiceDomain": "lockbox-v1-beta-1-i.hcs.io",
-//				"defaultOutgoingTelnetServer": "tcp-logger-v1-beta-1-i.hcs.io:8055"
-//			}
-//		}
-		
-		try {
-            JSONObject parent = new JSONObject();
-            JSONObject jsonObject = new JSONObject();
-
-            jsonObject.put("openpeer/common/application-name", "OpenPeer Native Sample App");
-            jsonObject.put("openpeer/common/application-image-url", "http://fake-image-url");
-            jsonObject.put("openpeer/common/application-url", "http://fake-application-url");
-            
-            Time expires = new Time ();
-            expires.set(30, 5, 2014);
-            //rel-dev2
-			//jsonObject.put("openpeer/calculated/authorizated-application-id", OPStack.createAuthorizedApplicationID("com.openpeer.nativeApp", "8f1ff375433b6e11026cb806a32ae4d04a59d7b1", expires ));
-			//lespaul
-            jsonObject.put("openpeer/calculated/authorizated-application-id", OPStack.createAuthorizedApplicationID("com.openpeer.nativeApp", "14b2c9df6713df465d97d0736863c42964faa678", expires ));
-			
-            jsonObject.put("openpeer/calculated/user-agent", "OpenPeerNativeSampleApp");
-            jsonObject.put("openpeer/calculated/device-id", Secure.getString(mContext.getContentResolver(),
-                    Secure.ANDROID_ID));
-            jsonObject.put("openpeer/calculated/os", android.os.Build.VERSION.RELEASE);
-            jsonObject.put("openpeer/calculated/system", android.os.Build.MODEL);
             parent.put("root", jsonObject);
             Log.d("output", parent.toString(2));
             return parent.toString(2);
--- openpeer-android-sdk/src/com/openpeer/app/OPSdkConfig.java
+++ openpeer-android-sdk/src/com/openpeer/app/OPSdkConfig.java
@@ -1,6 +1,19 @@
 package com.openpeer.app;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.openpeer.javaapi.OPStack;
+
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.provider.Settings.Secure;
+import android.text.format.Time;
+import android.util.Log;
 
 /**
  * 
@@ -13,11 +26,134 @@ import android.content.Context;
 public class OPSdkConfig {
 	// TODO add configuration items in
 	private static final String PATH_CONFIG_FILE = "openpeersdk.properties";
-	public static boolean debug = true;
+	private static final String PREFIX_APP_COMMON_SETTING = "openpeer/common/";
+	private static final String PREFIX_APP_CALCULATED_SETTING = "openpeer/calculated/";
 
-	public static void init(Context context) {
-		// if(config exists){
-		// }
+	private static final String KEY_APP_NAME = "application-name";
+	private static final String KEY_APP_IMAGE_URL = "application-image-url";
+	private static final String KEY_APP_APPLICATION_URL = "application-url";
+
+	private static final String KEY_OUTER_FRAME_URL = "outerFrameURL";
+	private static final String KEY_IDENTITY_PROVIDE_DOMAIN = "identityProviderDomain";
+	private static final String KEY_IDENTITY_BASE_URI = "identityFederateBaseURI";
+	private static final String KEY_NAMESPACE_GRANT_SERVICE_URL = "namespaceGrantServiceURL";
+
+	private static final long DURATION_ONE_MONTH_IN_MILLIS = 30 * 24 * 60 * 60
+			* 1000;
+
+	public static boolean debug = true;
+	private Context mContext;
+	private Properties mProperties = new Properties();
+	private static OPSdkConfig instance;
+
+	private OPSdkConfig() {
+
+	}
+
+	public static OPSdkConfig getInstance(Context context) {
+		if (instance == null) {
+			instance = new OPSdkConfig();
+			instance.mContext = context;
+			instance.init(context);
+		}
+		return instance;
+	}
+
+	public String getOuterFrameUrl() {
+		return mProperties.getProperty(KEY_OUTER_FRAME_URL);
+	}
+
+	public String getIdentityProviderDomain() {
+		return mProperties.getProperty(KEY_IDENTITY_PROVIDE_DOMAIN);
+	}
+
+	public String getIdentityBaseUri() {
+		return mProperties.getProperty(KEY_IDENTITY_BASE_URI);
+	}
+
+	public String getNamespaceGrantServiceUrl() {
+		return mProperties.getProperty(KEY_NAMESPACE_GRANT_SERVICE_URL);
+	}
+
+	public String getAPPSettingsString() {
+		try {
+			JSONObject parent = new JSONObject();
+			JSONObject jsonObject = new JSONObject();
+
+			Log.d("login", "getAPPSettingsString mProperties" + mProperties);
+			Log.d("login",
+					"getAPPSettingsString " + mProperties.propertyNames());
+
+			jsonObject.put(PREFIX_APP_COMMON_SETTING + KEY_APP_NAME,
+					mProperties.getProperty(KEY_APP_NAME));
+			jsonObject.put(PREFIX_APP_COMMON_SETTING + KEY_APP_IMAGE_URL,
+					mProperties.getProperty(KEY_APP_IMAGE_URL));
+			jsonObject.put(PREFIX_APP_COMMON_SETTING + KEY_APP_APPLICATION_URL,
+					mProperties.getProperty(KEY_APP_APPLICATION_URL));
+
+			Time expires = new Time();
+			expires.set(System.currentTimeMillis()
+					+ DURATION_ONE_MONTH_IN_MILLIS);
+			// expires.set(30, 6, 2014);
+			// rel-dev2
+			// jsonObject.put("openpeer/calculated/authorizated-application-id",
+			// OPStack.createAuthorizedApplicationID("com.openpeer.nativeApp",
+			// "8f1ff375433b6e11026cb806a32ae4d04a59d7b1", expires ));
+			// lespaul
+			jsonObject
+					.put("openpeer/calculated/authorizated-application-id",
+							OPStack.createAuthorizedApplicationID(
+									"com.openpeer.nativeApp",
+									"14b2c9df6713df465d97d0736863c42964faa678",
+									expires));
+
+			jsonObject.put("openpeer/calculated/user-agent",
+					"OpenPeerNativeSampleApp");
+			jsonObject.put("openpeer/calculated/device-id", Secure.getString(
+					mContext.getContentResolver(), Secure.ANDROID_ID));
+			jsonObject.put("openpeer/calculated/os",
+					android.os.Build.VERSION.RELEASE);
+			jsonObject
+					.put("openpeer/calculated/system", android.os.Build.MODEL);
+			parent.put("root", jsonObject);
+			Log.d("output", parent.toString(2));
+			return parent.toString(2);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "";
+		}
+
+	}
+
+	public void init(Context context) {
+		// try {
+		/**
+		 * getAssets() Return an AssetManager instance for your application's
+		 * package. AssetManager Provides access to an application's raw asset
+		 * files;
+		 */
+		AssetManager assetManager = context.getAssets();
+		/**
+		 * Open an asset using ACCESS_STREAMING mode. This
+		 */
+		InputStream inputStream;
+		try {
+			inputStream = assetManager.open(PATH_CONFIG_FILE);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		/**
+		 * Loads properties from the specified InputStream,
+		 */
+		try {
+			mProperties.load(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
