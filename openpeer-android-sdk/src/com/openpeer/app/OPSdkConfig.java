42a01e01a5dd92c74bb5e802f28b1164d9ded8d9
brucexia <bruce@hookflash.com>
2014-06-17 23:21:20 -0700

    fix crash after updating library

--- openpeer-android-sdk/src/com/openpeer/app/OPSdkConfig.java
+++ openpeer-android-sdk/src/com/openpeer/app/OPSdkConfig.java
@@ -113,6 +113,8 @@ public class OPSdkConfig {
 					android.os.Build.VERSION.RELEASE);
 			jsonObject
 					.put("openpeer/calculated/system", android.os.Build.MODEL);
+			jsonObject.put("openpeer/calculated/instance-id", java.util.UUID
+					.randomUUID().toString());
 			parent.put("root", jsonObject);
 			Log.d("output", parent.toString(2));
 			return parent.toString(2);
