bd36cb7bbd3b247eb01ac11c3a9c7b52227093e3
brucexia <bruce@hookflash.com>
2014-06-22 22:51:08 -0700

    #326 OPConversationThread use nativeClassPtr for equals method

--- openpeer-android-sdk/src/com/openpeer/javaapi/OPConversationThread.java
+++ openpeer-android-sdk/src/com/openpeer/javaapi/OPConversationThread.java
@@ -7,56 +7,59 @@ import android.text.format.Time;
 public class OPConversationThread {
 
 	private long nativeClassPointer;
-	
+
 	public static native String toString(MessageDeliveryStates state);
+
 	public static native String toString(ContactStates state);
 
-	public static native String toDebugString(OPConversationThread thread, boolean includeCommaPrefix);
-	
-	public static native OPConversationThread create(
-                                         OPAccount account,
-                                         List<OPIdentityContact> identityContacts
-                                         );
+	public static native String toDebugString(OPConversationThread thread,
+			boolean includeCommaPrefix);
 
-	public static native List<OPConversationThread> getConversationThreads(OPAccount account);
-	
+	public static native OPConversationThread create(OPAccount account,
+			List<OPIdentityContact> identityContacts);
+
+	public static native List<OPConversationThread> getConversationThreads(
+			OPAccount account);
+
 	public static native OPConversationThread getConversationThreadByID(
-                                                            OPAccount account,
-                                                            String threadID
-                                                            );
+			OPAccount account, String threadID);
+
 	public native long getStableID();
 
 	public native String getThreadID();
 
 	public native boolean amIHost();
-	
+
 	public native OPAccount getAssociatedAccount();
 
 	public native List<OPContact> getContacts();
 
-	public native List<OPIdentityContact> getIdentityContactList(OPContact contact);
-	
+	public native List<OPIdentityContact> getIdentityContactList(
+			OPContact contact);
+
 	public native ContactStates getContactState(OPContact contact);
 
-	public native void addContacts(List<OPContactProfileInfo> contactProfileInfos);
-	
+	public native void addContacts(
+			List<OPContactProfileInfo> contactProfileInfos);
+
 	public native void removeContacts(List<OPContact> contacts);
 
-    // sending a message will cause the message to be delivered to all the contacts currently in the conversation
-	public native void sendMessage(
-                             String messageID,
-                             String messageType,
-                             String message,
-                             boolean signMessage
-                             );
+	// sending a message will cause the message to be delivered to all the
+	// contacts currently in the conversation
+	public native void sendMessage(String messageID, String messageType,
+			String message, boolean signMessage);
 
-    // returns false if the message ID is not known
-	public native OPMessage getMessage(
-							String messageID
-                            );
+	// returns false if the message ID is not known
+	public native OPMessage getMessage(String messageID);
 
-    // returns false if the message ID is not known
-	public native MessageDeliveryStates getMessageDeliveryState(
-										 String messageID
-                                         );
+	// returns false if the message ID is not known
+	public native MessageDeliveryStates getMessageDeliveryState(String messageID);
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return o instanceof OPConversationThread
+				&& this.nativeClassPointer == ((OPConversationThread) o).nativeClassPointer;
+	}
+
 }
