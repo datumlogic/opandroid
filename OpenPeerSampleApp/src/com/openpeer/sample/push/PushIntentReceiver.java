32f401b20c869633f1f5aebf4d4029f894d7fcde
Bruce Xia <bruce@hookflash.com>
2014-08-14 11:41:36 -0700

    #525 fix NPE handling push registration when app is killed

--- OpenPeerSampleApp/src/com/openpeer/sample/push/PushIntentReceiver.java
+++ OpenPeerSampleApp/src/com/openpeer/sample/push/PushIntentReceiver.java
@@ -2,16 +2,16 @@
  *
  *  Copyright (c) 2014 , Hookflash Inc.
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
- *  
+ *
  *  1. Redistributions of source code must retain the above copyright notice, this
  *  list of conditions and the following disclaimer.
  *  2. Redistributions in binary form must reproduce the above copyright notice,
  *  this list of conditions and the following disclaimer in the documentation
  *  and/or other materials provided with the distribution.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,7 +22,7 @@
  *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  *  The views and conclusions contained in the software and documentation are those
  *  of the authors and should not be interpreted as representing official policies,
  *  either expressed or implied, of the FreeBSD Project.
@@ -123,8 +123,11 @@ public class PushIntentReceiver extends BroadcastReceiver {
         } else if (action.equals(PushManager.ACTION_REGISTRATION_FINISHED)) {
             String apid = intent.getStringExtra(PushManager.EXTRA_APID);
             Log.i(logTag, "Push registration finished " + apid);
-            if (apid != null && OPDataManager.getInstance().getSharedAccount().getState(0, null) == AccountStates.AccountState_Ready) {
-                OPPushManager.getInstance().associateDeviceToken(OPDataManager.getInstance().getSharedAccount().getPeerUri(), apid, new Callback<HackApiService.HackAssociateResult>() {
+            if (apid != null && OPDataManager.getInstance().getSharedAccount() != null
+                    && OPDataManager.getInstance().getSharedAccount().getState() == AccountStates.AccountState_Ready) {
+                OPPushManager.getInstance().associateDeviceToken(OPDataManager.getInstance().getSharedAccount().getPeerUri(),
+                        apid,
+                        new Callback<HackApiService.HackAssociateResult>() {
                     @Override
                     public void success(HackApiService.HackAssociateResult hackAssociateResult, Response response) {
 
