1efdee2eae3f0c92742a5695a6f006db6279fee1
Vladimir Morosev <mosa@hookflash.com>
2014-08-16 01:02:56 +0200

    - webrtc Java files moved out of OpenPeerSampleApp project https://github.com/openpeer/opandroid/issues/527

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ pushd opandroid/
 ./buildall_android.sh {PATH_TO_NDK}
 popd
 
-NOTE: It is required to have Android NDK r8e installed on the target machine. Path to the NDK is mandatory input for building 3rd party libraries. ninja build sysytem is required for WebRTC library build. You can install ninja using MacPorts or Homebrew (http://martine.github.io/ninja/).
+NOTE: It is required to have Android NDK r8e and Android SDK installed on the target machine (Android SDK comes with Eclipse ADT bundle). Paths to the NDK and SDK are mandatory input for building 3rd party libraries. ninja build sysytem is required for WebRTC library build. You can install ninja using MacPorts or Homebrew (http://martine.github.io/ninja/).
 
 
 2) From Eclipse ADT, import sdk project:
--- buildall_android.sh
+++ buildall_android.sh
@@ -28,17 +28,31 @@ esac
 #ndk-path
 if [[ $1 == *android-ndk-* ]]; then
 	echo "----------------- NDK Path is : $1 ----------------"
-	Input=$1;
+	Input1=$1;
 else
 	echo "Please enter your android ndk path:"
 	echo "For example:/home/astro/android-ndk-r8e"
-	read Input
-	echo "You entered:$Input"
+	read Input1
+	echo "You entered:$Input1"
+fi
+
+#sdk-path
+if [[ $2 == *android-sdk* ]]; then
+	echo "----------------- SDK Path is : $2 ----------------"
+	Input2=$2;
+elif [[ -n "$ANDROID_HOME" ]]; then
+    Input2=$ANDROID_HOME;
+	echo "----------------- SDK Path is : $ANDROID_HOME ----------------"
+else
+	echo "Please enter your android sdk path:"
+	echo "For example:/home/astro/android-sdk"
+	read Input2
+	echo "You entered:$Input2"
 fi
 
 #Set path
 echo "----------------- Exporting the android-ndk path ----------------"
-export PATH=$PATH:$Input:$Input/toolchains/arm-linux-androideabi-4.7/prebuilt/$HOST_OS-$ARCHTYPE/bin
+export PATH=$PATH:$Input1:$Input1/toolchains/arm-linux-androideabi-4.7/prebuilt/$HOST_OS-$ARCHTYPE/bin
 
 #build all modules
 echo "******************BUILD STARTED FOR ALL ORTC LIBS*******************"
@@ -46,67 +60,67 @@ echo "******************BUILD STARTED FOR ALL ORTC LIBS*******************"
 #boost build -- 1
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/boost/projects
-./build_boost_android.sh $Input
+./build_boost_android.sh $Input1
 popd
 
 #openssl build -- 2
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/openssl/projects/android
-./build-libssl_android.sh $Input
+./build-libssl_android.sh $Input1
 popd
 
 #curl build -- 3
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/curl/projects/android
-./build_curl_android.sh $Input
+./build_curl_android.sh $Input1
 popd
 
 #cryptopp build -- 4
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/cryptopp/projects/android-patches
-./build_cryptopp_android.sh $Input
+./build_cryptopp_android.sh $Input1
 popd
 
 #udns build -- 5
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/udns/projects/android
-./build_udns_android.sh $Input
+./build_udns_android.sh $Input1
 popd
 
 #zsLib build -- 6
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/zsLib/projects/android
-./build_zsLib_android.sh $Input
+./build_zsLib_android.sh $Input1
 popd
 
 #op-services-cpp build -- 7
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/op-services-cpp/projects/android
-./build_op-services-cpp_android.sh $Input
+./build_op-services-cpp_android.sh $Input1
 popd
 
 #webrtc build -- 8
 pushd `pwd`
 cd libs/op/libs/ortc-lib/libs/webrtc/projects/android
-./build_webrtc_android.sh $Input
+./build_webrtc_android.sh $Input1 $Input2
 popd
 
 #op-stack-cpp build -- 9
 pushd `pwd`
 cd libs/op/libs/op-stack-cpp/projects/android
-./build_op-stack-cpp_android.sh $Input
+./build_op-stack-cpp_android.sh $Input1
 popd
 
 #op-core-cpp build - 10
 pushd `pwd`
 cd libs/op/libs/op-core-cpp/projects/android
-./build_op-core-cpp_android.sh $Input
+./build_op-core-cpp_android.sh $Input1
 popd
 
 #openpeer-android-sdk shared build - 11
 pushd `pwd`
 cd openpeer-android-sdk
-./build_openpeer_android_sdk_android.sh $Input
+./build_openpeer_android_sdk_android.sh $Input1
 popd
 
 echo "******************BUILD COMPLETED FOR ALL ANDROID SDK LIBS*******************"
