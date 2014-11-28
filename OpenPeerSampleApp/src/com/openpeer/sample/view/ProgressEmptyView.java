06af7b4501bac821ab268bc7c31f6426d27f0b43
Bruce Xia <bruce@hookflash.com>
2014-11-27 18:12:07 -0800

    #665 add ProgressEmptyView

---
+++ OpenPeerSampleApp/src/com/openpeer/sample/view/ProgressEmptyView.java
@@ -0,0 +1,83 @@
+/*
+ * ******************************************************************************
+ *  *
+ *  *  Copyright (c) 2014 , Hookflash Inc.
+ *  *  All rights reserved.
+ *  *
+ *  *  Redistribution and use in source and binary forms, with or without
+ *  *  modification, are permitted provided that the following conditions are met:
+ *  *
+ *  *  1. Redistributions of source code must retain the above copyright notice, this
+ *  *  list of conditions and the following disclaimer.
+ *  *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  *  this list of conditions and the following disclaimer in the documentation
+ *  *  and/or other materials provided with the distribution.
+ *  *
+ *  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  *
+ *  *  The views and conclusions contained in the software and documentation are those
+ *  *  of the authors and should not be interpreted as representing official policies,
+ *  *  either expressed or implied, of the FreeBSD Project.
+ *  ******************************************************************************
+ */
+
+package com.openpeer.sample.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.openpeer.sample.R;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+
+@EViewGroup(R.layout.layout_progress_empty_view)
+public class ProgressEmptyView extends LinearLayout{
+    @ViewById
+    View progressBar;
+    @ViewById
+    TextView messageView;
+
+    public ProgressEmptyView(Context context) {
+        this(context,null,0);
+    }
+
+    public ProgressEmptyView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public ProgressEmptyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setMessage(String message) {
+        messageView.setText(message);
+    }
+
+    public void setMessage(int resId) {
+        messageView.setText(resId);
+    }
+
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        messageView.setVisibility(View.GONE);
+    }
+
+    public void showText() {
+        progressBar.setVisibility(View.GONE);
+        messageView.setVisibility(View.VISIBLE);
+    }
+
+}
