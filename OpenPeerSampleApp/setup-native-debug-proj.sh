349302adcaddf56dfbd9503bbf32cedf25bfd2c7
Bruce Xia <bruce@hookflash.com>
2014-12-08 10:02:54 -0800

    update setup-native-debug-proj.sh to link to sdk assets

--- OpenPeerSampleApp/setup-native-debug-proj.sh
+++ OpenPeerSampleApp/setup-native-debug-proj.sh
@@ -1,3 +1,5 @@
+ln -s ../openpeer-android-sdk/assets/openpeer_database.sql assets/openpeer_database.sql
+
 ln -s ../../../../openpeer-android-sdk/src/com/openpeer/javaapi src/com/openpeer/javaapi
 ln -s ../../../../openpeer-android-sdk/src/com/openpeer/sdk src/com/openpeer/sdk
 ln -s ../openpeer-android-sdk/jni jni
