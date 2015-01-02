c325a3365528de8b2a9eab8b279e7add31a80e94
Bruce Xia <bruce@hookflash.com>
2015-01-02 08:48:06 -0800

    #590 add system message parser

--- openpeer-android-sdk/src/com/openpeer/sdk/model/SystemMessage.java
+++ openpeer-android-sdk/src/com/openpeer/sdk/model/SystemMessage.java
@@ -1,6 +1,15 @@
 package com.openpeer.sdk.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 /**
  * {"system":{"callStatus":{"$id":"adf","status":"placed","mediaType":"audio",
@@ -10,6 +19,10 @@ public class SystemMessage<T> {
 
     T system;
 
+    public T getSystemObject(){
+        return system;
+    }
+
     public SystemMessage() {
     }
 
@@ -17,13 +30,40 @@ public class SystemMessage<T> {
         system = t;
     }
 
-    public static SystemMessage<CallSystemMessage> parseCallSystemMessage(String jsonBlob) {
-        return GsonFactory.getGson().fromJson(jsonBlob,
-                                              new TypeToken<SystemMessage<CallSystemMessage>>() {
-                                              }.getType());
+    public static SystemMessage parseSystemMessage(String jsonBlob) {
+        return getGson().fromJson(jsonBlob,
+                                  SystemMessage.class);
     }
 
-    public String toString(){
+    public String toString() {
         return GsonFactory.getGson().toJson(this);
+    }
+
+    static class SystemMessageDeserializer implements JsonDeserializer<SystemMessage> {
+        @Override
+        public SystemMessage deserialize(JsonElement json, Type typeOfT,
+                                         JsonDeserializationContext context) throws
+            JsonParseException {
+            SystemMessage message = new SystemMessage();
+            JsonObject object = json.getAsJsonObject().getAsJsonObject("system");
+            System.out.println("object " + object.toString());
+            if (object.has("callStatus")) {
+
+                CallSystemMessage callSystemMessage = context.deserialize(object, CallSystemMessage.class);
+                message.system = callSystemMessage;
+            }
+            return message;
+        }
+    }
+
+    private static Gson gson;
+
+    public static Gson getGson() {
+        if (gson == null) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(SystemMessage.class, new SystemMessageDeserializer());
+            gson = gsonBuilder.create();
+        }
+        return gson;
     }
 }
